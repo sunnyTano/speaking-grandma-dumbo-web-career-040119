f8deff1982c5d1f762f83748efa93780347c118c
Susan Tan <ssusantan9@gmail.com>
2019-03-12 03:38:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 elseif phrase.upcase
   return "NO, NOT SINCE 1938!"
 else 
-  return 
+  return "HUH?! SPEAK UP, SONNY!"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
